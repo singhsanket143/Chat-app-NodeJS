d9b263add51c016b78a1262911b14f1345ef5408
Sanket Singh <singhsanket143@gmail.com>
2023-02-12 15:57:48 +0530

    Added codes

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
-    socket.on('from_client', () => {
-        console.log("event coming from client");
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        // io.emit('msg_rcvd', data);
+        // socket.emit('msg_rcvd', data)
+        socket.broadcast.emit('msg_rcvd', data)
     })
 
-    setInterval(() => {
-        socket.emit('from_server');
-    }, 2000);
 });
 
 app.use('/', express.static(__dirname + '/public'));
